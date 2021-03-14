d0f41d36859e20cd1e93e388d2d5533bf85f1852
Mridul2820 <immridulpanda2820@gmail.com>
2021-03-14 13:22:19 +0530

    env set up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 
 import React from 'react'
 
+const mainURL = `api.openweathermap.org/data/2.5/`
+const apiID = `&appid=${process.env.REACT_APP_API_KEY}`
+
 import './styles/app.scss'
 
 const App = () => {
