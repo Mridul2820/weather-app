cbb792453e9d2b66bdf74dce2bd1104c42e39358
Mridul2820 <immridulpanda2820@gmail.com>
2021-03-14 19:53:51 +0530

    dummy ui style

--- src/App.js
+++ src/App.js
@@ -8,6 +8,18 @@ const apiID = `&appid=${process.env.REACT_APP_API_KEY}`
 
 
 const App = () => {
+    const dateBuilder = d => {
+        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+        let day = days[d.getDay()]
+        let date = d.getDate()
+        let month = months[d.getMonth()]
+        let year = d.getFullYear()
+
+        return `${day} ${date} ${month} ${year}`
+    }
+
     return (
         <div className='app'>
             <main>
@@ -17,7 +29,21 @@ const App = () => {
                         className="search-bar"
                         placeholder="Search..."
                     />
+                </div>
+                <div className="location-box">
+                    <div className="location">Kolkata, India</div>
+                    <div className="date">
+                        {dateBuilder(new Date())}
                     </div>
+                </div>
+                <div className="weather-box">
+                    <div className="temp">
+                        15 C
+                    </div>
+                    <div className="weather">
+                        Sunny
+                    </div>
+                </div>
             </main>
         </div>
     )
