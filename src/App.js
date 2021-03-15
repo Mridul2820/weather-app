b5800d6882b42883aa62f4dae1ed616d765e96f6
Mridul2820 <immridulpanda2820@gmail.com>
2021-03-15 11:59:15 +0530

    more weather info added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 
+import LocationBox from './components/LocationBox'
+import WeatherBox from './components/WeatherBox'
 import './styles/app.scss'
 
 const mainURL = 'https://api.openweathermap.org/data/2.5/weather'
@@ -18,18 +20,6 @@ const App = () => {
                 console.log(result);
             })
         }
-    }
-
-    const dateBuilder = d => {
-        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-        let day = days[d.getDay()]
-        let date = d.getDate()
-        let month = months[d.getMonth()]
-        let year = d.getFullYear()
-
-        return `${day} ${date} ${month} ${year}`
     }
 
     return (
@@ -51,20 +41,8 @@ const App = () => {
                 </div>
                 {(typeof weather.main != "undefined") ? (
                 <div>
-                    <div className="location-box">
-                        <div className="location">{weather.name}, {weather.sys.country} </div>
-                        <div className="date">
-                            {dateBuilder(new Date())}
-                        </div>
-                    </div>
-                    <div className="weather-box">
-                        <div className="temp">
-                            {Math.round(weather.main.temp)}°C
-                        </div>
-                        <div className="weather">
-                            {weather.weather[0].main}
-                        </div>
-                    </div>                    
+                    <LocationBox weather={weather} />
+                    <WeatherBox  weather={weather} />
                 </div>
                 ) : ''}
             </main>
